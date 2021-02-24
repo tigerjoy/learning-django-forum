d8d4159440f893c01346c72b26cf9785a6cb2075
Ranajoy Saha <ranajoy123saha@gmail.com>
2021-02-24 22:17:23 +0530

    Add the naive way of receiving post data from new_topics view

--- myproject/boards/views.py
+++ myproject/boards/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import get_object_or_404, render
-from django.http import Http404
-from .models import Board
+from django.shortcuts import get_object_or_404, render, redirect
+from django.contrib.auth.models import User
+from .models import Board, Topic, Post
 
 def home(request):
     boards = Board.objects.all()
@@ -41,4 +41,28 @@ def board_topics(request, pk):
 
 def new_topic(request, pk):
     board = get_object_or_404(Board, pk=pk)
+
+    if request.method == 'POST':
+        subject = request.POST["subject"]
+        message = request.POST["message"]
+
+        #TODO: get the currently logged in user
+        user = User.objects.first()
+
+        topic = Topic.objects.create(
+            subject=subject,
+            board=board,
+            starter=user
+        )
+
+        post = Post.objects.create(
+            message=message,
+            created_by=user,
+            topic=topic
+        )
+
+        #TODO: redirect to the created topic
+        return redirect('board_topics', pk=board.pk) 
+
+
     return render(request, 'new_topic.html', {'board': board})
