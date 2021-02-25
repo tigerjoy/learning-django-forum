96619e4094eb7758e6fc360fbfa0605d4bae48c0
Ranajoy Saha <ranajoy123saha@gmail.com>
2021-02-25 17:23:21 +0530

    Improve signup() view to validate and save user signup information

--- myproject/accounts/views.py
+++ myproject/accounts/views.py
@@ -1,5 +1,18 @@
-from django.shortcuts import render
+from django.contrib.auth import login as auth_login
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 
 # Create your views here.
 def signup(request):
-    return render(request, 'signup.html')
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            auth_login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm(request.POST)
+    return render(request, 'signup.html', context={"form": form})
+
+
+
