76e1e373df1ecf330f3ea275149ed4fa93c62037
Ranajoy Saha <ranajoy123saha@gmail.com>
2021-02-25 11:19:41 +0530

    Updated new_topic() view to use the NewTopicForm, ModelForm

--- myproject/boards/views.py
+++ myproject/boards/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth.models import User
+from .forms import NewTopicForm
 from .models import Board, Topic, Post
+
 
 def home(request):
     boards = Board.objects.all()
-    
+
     return render(request, 'home.html', context={"boards": boards})
 
 # Rendering HTML without using render()
@@ -15,10 +17,11 @@ def home(request):
 
 #     for board in boards:
 #         board_names.append(board.name)
-    
+
 #     response_html = "<br>".join(board_names)
 
 #     return HttpResponse(response_html)
+
 
 def board_topics(request, pk):
     board = get_object_or_404(Board, pk=pk)
@@ -39,30 +42,61 @@ def board_topics(request, pk):
 
 #     return render(request, 'topics.html', context)
 
+# Django way of handling forms
+
+
 def new_topic(request, pk):
     board = get_object_or_404(Board, pk=pk)
+    # TODO: get the currently logged in user
+    user = User.objects.first()
 
     if request.method == 'POST':
-        subject = request.POST["subject"]
-        message = request.POST["message"]
+        form = NewTopicForm(request.POST)
 
-        #TODO: get the currently logged in user
-        user = User.objects.first()
+        if form.is_valid():
+            # Do not save the form data yet to the
+            # database
+            topic = form.save(commit=False)
+            topic.board = board
+            topic.starter = user
+            topic.save()
+            post = Post.objects.create(
+                message=form.cleaned_data.get('message'),
+                topic=topic,
+                created_by=user
+            )
 
-        topic = Topic.objects.create(
-            subject=subject,
-            board=board,
-            starter=user
-        )
+            # TODO: redirect to the created topic
+            return redirect('board_topics', pk=board.pk)
+    else:
+        form = NewTopicForm()
 
-        post = Post.objects.create(
-            message=message,
-            created_by=user,
-            topic=topic
-        )
+    return render(request, 'new_topic.html', {'board': board, 'form': form})
 
-        #TODO: redirect to the created topic
-        return redirect('board_topics', pk=board.pk) 
+# Naive way to handling forms
+# def new_topic(request, pk):
+#     board = get_object_or_404(Board, pk=pk)
 
+#     if request.method == 'POST':
+#         subject = request.POST["subject"]
+#         message = request.POST["message"]
 
-    return render(request, 'new_topic.html', {'board': board})
+#         #TODO: get the currently logged in user
+#         user = User.objects.first()
+
+#         topic = Topic.objects.create(
+#             subject=subject,
+#             board=board,
+#             starter=user
+#         )
+
+#         post = Post.objects.create(
+#             message=message,
+#             created_by=user,
+#             topic=topic
+#         )
+
+#         #TODO: redirect to the created topic
+#         return redirect('board_topics', pk=board.pk)
+
+#     return render(request, 'new_topic.html', {'board': board})
