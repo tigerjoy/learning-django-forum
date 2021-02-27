910c67e73242329c155c7d96b4f8f7ab8f2c37f6
Ranajoy Saha <ranajoy123saha@gmail.com>
2021-02-27 07:34:14 +0530

    Add path logout/ and used LogoutView

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 from boards import views
 from accounts import views as accounts_views
@@ -22,6 +23,7 @@ from accounts import views as accounts_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('signup/', accounts_views.signup, name="signup"),
+    path('logout/', auth_views.LogoutView.as_view(), name="logout"),
     path('', views.home, name="home"),
     path('boards/<int:pk>', views.board_topics, name="board_topics"),
     path('boards/<int:pk>/new', views.new_topic, name="new_topic"),
