94927020b7cab6c2fb5e9bb0804e7a1549d203b2
Ranajoy Saha <ranajoy123saha@gmail.com>
2021-02-27 07:03:33 +0530

    Change signup() view to use SignUpForm instead of default UserCreationForm

--- myproject/accounts/views.py
+++ myproject/accounts/views.py
@@ -1,18 +1,32 @@
 from django.contrib.auth import login as auth_login
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
+from .forms import SignUpForm
 
 # Create your views here.
+# Uses the SignUpForm with added email field
 def signup(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = SignUpForm(request.POST)
         if form.is_valid():
             user = form.save()
             auth_login(request, user)
             return redirect('home')
     else:
-        form = UserCreationForm(request.POST)
+        form = SignUpForm()
     return render(request, 'signup.html', context={"form": form})
+
+# Uses the UserCreationForm
+# def signup(request):
+#     if request.method == 'POST':
+#         form = UserCreationForm(request.POST)
+#         if form.is_valid():
+#             user = form.save()
+#             auth_login(request, user)
+#             return redirect('home')
+#     else:
+#         form = UserCreationForm(request.POST)
+#     return render(request, 'signup.html', context={"form": form})
 
 
 
