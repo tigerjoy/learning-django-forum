8c18706a964028f0f6f73abfa6a86a92cdfdab2d
Ranajoy Saha <ranajoy123saha@gmail.com>
2021-02-28 18:38:59 +0530

    Fix logout/ view being named login and login/ view being named logout

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -23,8 +23,8 @@ from accounts import views as accounts_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('signup/', accounts_views.signup, name="signup"),
-    path('logout/', auth_views.LoginView.as_view(template_name="login.html"), name="login"),
-    path('login/', auth_views.LogoutView.as_view(), name="logout"),
+    path('login/', auth_views.LoginView.as_view(template_name="login.html"), name="login"),
+    path('logout/', auth_views.LogoutView.as_view(), name="logout"),
     path('', views.home, name="home"),
     path('boards/<int:pk>', views.board_topics, name="board_topics"),
     path('boards/<int:pk>/new', views.new_topic, name="new_topic"),
